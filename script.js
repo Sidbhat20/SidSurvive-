b8e78cb06d1fd5ccfb5e3f59c57546be19cdfac0
Sidbhat20 <sidbhat20@gmail.com>
2025-10-23 14:50:55 +0530

    Fix image paths with relative URLs

--- script.js
+++ script.js
@@ -84,8 +84,8 @@ function init() {
     fragmentShader: displayFragmentShader,
   });
 
-  loadImage("/SidSurvive-/portrait_top.jpg", topTexture, topTextureSize);
-  loadImage("/SidSurvive-/portrait_bottom.jpg", bottomTexture, bottomTextureSize);
+  loadImage("./portrait_top.jpg", topTexture, topTextureSize);
+  loadImage("./portrait_bottom.jpg", bottomTexture, bottomTextureSize);
 
   const planeGeometry = new THREE.PlaneGeometry(2, 2);
   const displayMesh = new THREE.Mesh(planeGeometry, displayMaterial);
